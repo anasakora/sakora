c1190a52a7454f2f0db2b42eb7f1d794f341d71d
anasakora <53491903+anasakora@users.noreply.github.com>
2019-08-02 15:10:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ console.log("For IIahmad111");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("606195535325691925")
+let channel =     client.channels.get("606850206465523716")
 setInterval(function() {
 channel.send(`Helen Is Sad :sob: But She Is Love You :heart: `);
 }, 30)
