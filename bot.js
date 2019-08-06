d1e9e1b71ef9d120bb00e54e5220bd678e22904e
anasakora <53491903+anasakora@users.noreply.github.com>
2019-08-06 19:46:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ console.log("For IIahmad111");
 client.on("ready", () => {
 let channel =     client.channels.get("606577146919649301")
 setInterval(function() {
-channel.send(`من طرف الزبال رشدي @everyone`);
+channel.send(`#ping رشدي الزبال`);
 }, 30)
 })
 
